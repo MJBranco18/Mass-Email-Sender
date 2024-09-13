49b5781b796d45fca3fa191a4f5c194d822f0984
Mário Branco <mario.branco18@gmail.com>
2024-09-13 16:37:56 +0100

    First version

---
+++ src/app/EmailEditor.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function EmailEditor() {
+  return (
+    <div style={{ maxWidth: '1000px', margin: 'auto' }}>
+      <iframe
+        src="/index.html"
+        style={{ width: '100%', height: '100vh', border: 'none' }}
+      ></iframe>
+    </div>
+  );
+}
